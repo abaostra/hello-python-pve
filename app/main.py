49ce618cfd08f0b043b7133c7e8c7c3cc92585ce
abaostra <eraman.ab@gmail.com>
2024-12-19 13:42:05 -0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Python!"
+    return "Hello from Aman Basotra, Tanzu-PVE-Team, 1st Sample Python Based app!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
